eccde8855ace9d7abc063ab2bc31b9f91f36abbd
Abhishek Singh <meabhisingh25@gmail.com>
2022-09-16 06:20:02 +0530

    Logout Fixed

--- controllers/user.js
+++ controllers/user.js
@@ -12,7 +12,11 @@ export const myProfile = (req, res, next) => {
 export const logout = (req, res, next) => {
   req.session.destroy((err) => {
     if (err) return next(err);
-    res.clearCookie("connect.sid");
+    res.clearCookie("connect.sid", {
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
+    });
     res.status(200).json({
       message: "Logged Out",
     });
@@ -45,10 +49,10 @@ export const getAdminStats = asyncError(async (req, res, next) => {
     success: true,
     usersCount,
     ordersCount: {
-      total:orders.length,
-      preparing:preparingOrders.length,
-      shipped:shippedOrders.length,
-      delivered:deliveredOrders.length
+      total: orders.length,
+      preparing: preparingOrders.length,
+      shipped: shippedOrders.length,
+      delivered: deliveredOrders.length,
     },
     totalIncome,
   });
